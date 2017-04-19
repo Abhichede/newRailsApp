321a8d3079e7ed9435b90586ed50a85cb4171eef
abhichede777 <abhichede777@gmail.com>
2017-04-19 11:33:11 +0530

    added on screen payment option.

--- db/migrate/20170418081116_add_column_to_booking_details.rb
+++ db/migrate/20170418081116_add_column_to_booking_details.rb
@@ -1,5 +1,5 @@
 class AddColumnToBookingDetails < ActiveRecord::Migration[5.0]
   def change
-    add_column :'booking_details.js', :paid_amount, :string
+    add_column :'booking_details', :paid_amount, :string
   end
 end
