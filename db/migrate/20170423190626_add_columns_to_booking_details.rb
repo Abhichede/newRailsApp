dbbcfb13e5147f604840fe5560ff05e897b32516
abhichede777 <abhichede777@gmail.com>
2017-04-24 17:45:00 +0530

    updaated additional customer info.

--- db/migrate/20170423190626_add_columns_to_booking_details.rb
+++ db/migrate/20170423190626_add_columns_to_booking_details.rb
@@ -1,7 +1,5 @@
 class AddColumnsToBookingDetails < ActiveRecord::Migration[5.0]
   def change
-    add_column :booking_details, :MSEB_charges, :string
-    add_column :booking_details, :other_charges, :string
     add_column :booking_details, :water_charges, :string
     add_column :booking_details, :parking_charges, :string
     add_column :booking_details, :maintenance_charges, :string
