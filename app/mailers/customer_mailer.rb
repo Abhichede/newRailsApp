fe7cfcefdbec8cb237d0e5c949a66e734a673762
abhichede777 <abhichede777@gmail.com>
2017-04-29 19:58:11 +0530

    mailers added.

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,8 +1,7 @@
 class CustomerMailer < ApplicationMailer
   default from: 'abhichede777@gmail.com'
 
-  def welcome_email(user)
-    @user = user
+  def welcome_email()
     mail(to: 'abhijitchede777@gmail.com', subject: 'Welcome to My Awesome Site')
   end
 end
