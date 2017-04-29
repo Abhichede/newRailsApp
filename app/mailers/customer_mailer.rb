fab84ce995ae3930ea8f34302f835933b0ef8220
abhichede777 <abhichede777@gmail.com>
2017-04-29 20:15:05 +0530

    mailers added.

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,5 +1,5 @@
 class CustomerMailer < ApplicationMailer
-  default from: 'abhichede777@gmail.com'
+  default from: 'uniquesoftdevelopers@gmail.com'
 
   def welcome_email()
     mail(to: 'abhijitchede777@gmail.com', subject: 'Welcome to My Awesome Site')
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => 'abhichede777@gmail.com',
+      :user_name            => 'uniquesoftdevelopers@gmail.com',
       :password             => 'Abhichede777@',
       :authentication       => "plain",
       :enable_starttls_auto => true
