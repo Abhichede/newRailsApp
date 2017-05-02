fe2df2970edb66ffa3c0125e6005429fdb064049
abhichede777 <abhichede777@gmail.com>
2017-05-03 00:11:20 +0530

    Mailer notifications.

--- app/models/booking_detail.rb
+++ app/models/booking_detail.rb
@@ -3,7 +3,7 @@ class BookingDetail < ApplicationRecord
   belongs_to :flat
   has_many :payment_details
 
-  def payment_schedule
+  def self.payment_schedule
     BookingDetailsMailer.dummy_mail.deliver
   end
 end
