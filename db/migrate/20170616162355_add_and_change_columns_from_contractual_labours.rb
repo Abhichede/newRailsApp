6db65277b489c0ea1a2d615a3cc2b7fd47e5b224
abhichede777 <abhichede777@gmail.com>
2017-06-23 18:35:38 +0530

    new modules added.

--- db/migrate/20170616162355_add_and_change_columns_from_contractual_labours.rb
+++ db/migrate/20170616162355_add_and_change_columns_from_contractual_labours.rb
@@ -1,5 +1,6 @@
 class AddAndChangeColumnsFromContractualLabours < ActiveRecord::Migration[5.0]
   def change
     add_column :contractual_labours, :site_id,:integer
+    change_column :contractual_labours, :contractor_id, :integer
   end
 end
--- db/migrate/20170616162805_change_columns_from_contractual_labours.rb
+++ db/migrate/20170616162805_change_columns_from_contractual_labours.rb
@@ -1,6 +1,5 @@
 class ChangeColumnsFromContractualLabours < ActiveRecord::Migration[5.0]
   def change
-    #rename_column :contractual_labours, :name_of_contractor, :contractor_id
-    change_column :contractual_labours, :contractor_id, :integer
+    rename_column :contractual_labours, :name_of_contractor, :contractor_id
   end
 end
