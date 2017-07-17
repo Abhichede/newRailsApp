db67937607149306d90c51dc2ad2b47a5e008c50
abhichede777 <abhichede777@gmail.com>
2017-07-17 10:11:38 +0530

    new cdn for jquery validate and investment returns modified.

--- db/migrate/20170616162355_add_and_change_columns_from_contractual_labours.rb
+++ db/migrate/20170616162355_add_and_change_columns_from_contractual_labours.rb
@@ -1,6 +1,6 @@
 class AddAndChangeColumnsFromContractualLabours < ActiveRecord::Migration[5.0]
   def change
-    add_column :contractual_labours, :site_id,:integer
-    change_column :contractual_labours, :contractor_id, :integer
+     add_column :contractual_labours, :site_id,:integer
+
   end
 end
