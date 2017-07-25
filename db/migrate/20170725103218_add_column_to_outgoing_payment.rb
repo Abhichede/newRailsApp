8c3169392dc34040e6b21226f53147377bba8d36
abhichede777 <abhichede777@gmail.com>
2017-07-25 19:18:56 +0530

    issue with material payments was resolved, and date formats changed, new field added in contractual_labour 'payment_for_id'

--- db/migrate/20170725103218_add_column_to_outgoing_payment.rb
+++ db/migrate/20170725103218_add_column_to_outgoing_payment.rb
@@ -1,5 +1,5 @@
 class AddColumnToOutgoingPayment < ActiveRecord::Migration[5.0]
   def change
-    add_column :outgoing_payments, :payment_for_id, :integer, null: false
+    add_column :outgoing_payments, :payment_for_id, :integer
   end
 end
