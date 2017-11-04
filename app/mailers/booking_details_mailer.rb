b08fdccdbe568c4bb0a8fad13c71e4ac640177e4
abhichede777 <abhichede777@gmail.com>
2017-11-04 18:11:05 +0530

    Mailers are activated and some changes in customer booking for stamp duty

--- app/mailers/booking_details_mailer.rb
+++ app/mailers/booking_details_mailer.rb
@@ -1,8 +1,8 @@
 ## Customer Booking mailer class
 class BookingDetailsMailer < ApplicationMailer
-  @users = User.where(role: 'ADMIN')
   def booking_details_mail(booking_details)
     @booking_detail = booking_details
+    @users = User.where(role: 'ADMIN')
     @users.each do |user|
       mail(to: user.email, subject: 'Booking Details')
     end
@@ -10,6 +10,7 @@ class BookingDetailsMailer < ApplicationMailer
 
   def payment_details_mail(payment_details)
     @payment_detail = payment_details
+    @users = User.where(role: 'ADMIN')
     @users.each do |user|
       mail(to: user.email, subject: 'Payment Details')
     end
@@ -17,12 +18,14 @@ class BookingDetailsMailer < ApplicationMailer
 
   def schedule_next_installment_mail(booking_details)
     @booking_detail = booking_details
+    @users = User.where(role: 'ADMIN')
     @users.each do |user|
       mail(to: user.email, subject: 'Payment Details')
     end
   end
 
   def dummy_mail
+    @users = User.where(role: 'ADMIN')
     @users.each do |user|
        mail(to: user.email, subject: 'Dummy Mail')
      end
