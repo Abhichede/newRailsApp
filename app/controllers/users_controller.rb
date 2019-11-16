4bbd0ca893331fbe429cf156ec7e6f2fd2e2ecff
Abhijit Chede <abhijit.chede@techverito.com>
2019-11-16 23:03:58 +0530

    changed the toggling logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
   before_action :authorised?
-  before_action :find_user, only: %i[toggle_user_status]
 
   def index
     @users = User.paginate(:page => params[:page], :per_page => 10).order(:role)
   end
 
   def toggle_user_status
-    active_status = @user.active?
-    @user.update!(active: !active_status)
+    @user =  User.find(params['id'])
+    @user.update!(active: !@user.active?)
     respond_to do |format|
       format.html
       format.json { 
@@ -21,8 +20,4 @@ class UsersController < ApplicationController
     user.is_online
   end
 
-  private
-  def find_user
-   @user =  User.find(params['id']) if params['id'].present?
-  end
 end
