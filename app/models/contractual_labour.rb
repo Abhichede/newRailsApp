848dcc1fe89f991100cea1538b05cc1459a6c09d
Abhijit Chede <abhijit.chede@techverito.com>
2020-01-18 13:24:34 +0530

    sorting the list by date

--- app/models/contractual_labour.rb
+++ app/models/contractual_labour.rb
@@ -3,10 +3,10 @@ class ContractualLabour < ApplicationRecord
   belongs_to :contractor
 
   def outgoing_payments
-    OutgoingPayment.where(:payment_for => 'CONTRACTOR', :payment_for_id => id).order("created_at DESC").map(&:serializable_hash)
+    OutgoingPayment.where(:payment_for => 'CONTRACTOR', :payment_for_id => id).order("Date(date) DESC").map(&:serializable_hash)
   end
 
   def outgoing_payments_by_site site_id
-    OutgoingPayment.where(:payment_for => 'CONTRACTOR', :payment_for_id => id, site_id: site_id).order("created_at DESC").map(&:serializable_hash)
+    OutgoingPayment.where(:payment_for => 'CONTRACTOR', :payment_for_id => id, site_id: site_id).order("Date(date) DESC").map(&:serializable_hash)
   end
 end
