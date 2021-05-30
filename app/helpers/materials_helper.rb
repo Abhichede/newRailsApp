61b85a213d02abd7bef169f64adef70f77a20d04
Abhijit Chede <abhijit.chede@techverito.com>
2021-05-30 19:20:48 +0530

    Showing material totals

--- app/helpers/materials_helper.rb
+++ app/helpers/materials_helper.rb
@@ -1,2 +1,13 @@
 module MaterialsHelper
+  ActionView::Helpers::NumberHelper
+  def get_quantity site, type
+    quantity = 0
+    total_amount = 0
+    site.materials.where(type_of_material: type).each do |material_quantity|
+      quantity += material_quantity.quantity.to_f
+      total_amount += material_quantity.amount.to_f
+    end
+    total_amount = number_to_currency(total_amount, unit: "₹", precision: 2, delimiter: ',')
+    return quantity, total_amount
+  end
 end
