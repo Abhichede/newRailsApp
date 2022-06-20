5ed27e7f661c193a041f1b6988cb3c60ecb3baf5
Abhijit Chede <abhijit.chede@techverito.com>
2022-06-20 18:00:29 +0530

    Handles the shops and flats calculations

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,4 +2,8 @@ class Flat < ApplicationRecord
 
   belongs_to :site
   has_one :booking_detail, dependent: :destroy
+
+  scope :shops, -> { where("flat_type LIKE lower('%?%')", "shop")}
+  scope :booked_shops, -> { where("lower(flat_type) LIKE ? AND booking_status = ?", "%shop%", "1")}
+  scope :booked_flats, -> { where(:booking_status => 1) }
 end
