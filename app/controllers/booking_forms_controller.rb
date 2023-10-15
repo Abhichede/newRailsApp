88f5bd25e1dfe89fa6844c2a15fb7036769718ea
Abhijit Chede <abhijit.chede@techverito.com>
2023-10-15 18:24:29 +0530

    Removes a4 config

--- app/controllers/booking_forms_controller.rb
+++ app/controllers/booking_forms_controller.rb
@@ -13,7 +13,7 @@ class BookingFormsController < ApplicationController
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: "#{@booking_form.name}", page_size: "A4"
+        render pdf: "#{@booking_form.name}"
       end
     end
   end
